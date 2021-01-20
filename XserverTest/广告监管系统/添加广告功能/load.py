42319599f383710c40b9652ae955b572e3a4d129
Questyou <maitian3352@163.com>
2021-01-20 23:58:17 +0800

    样本添加接口主要功能

--- XserverTest/广告监管系统/添加广告功能/load.py
+++ XserverTest/广告监管系统/添加广告功能/load.py
@@ -1,19 +1,57 @@
-import  requests
+import  requests  , os
+
+from moviepy.editor import VideoFileClip
+
+path = 'E:\\STE\\Xserver\\Xserver2.7\\ADSample\\'
+addsampleurl = 'http://172.17.13.173:12521/advertisementSample/saveOrUpdateAdvertisementSample'
+token = '8a8af3a3-64cb-48b5-84f0-800370c05261'
 
 
-def addsample(url):
-    # url = 'http://172.17.13.172:12580/xserver/station/uploadStationPictures?stationId=1160'
+filename123 = "E:\\STE\\Xserver\\Xserver2.7\\ADSample\\anquanjiashi.mp4"
+
+def get_file_times(filename):
+    clip = VideoFileClip(filename)
+    file_time = clip.duration
+    file_time_ms = int(file_time * 1000 )
+    return  file_time_ms
+
+
+
+
+def get_file_nameTest(path):
+    # names = os.fchdir(path)
+    # for name in names:
+        #获取到文件路径
+    filepath = path
+        # 打开文件
+    file = {'file': open(filepath, 'rb')}
+        #获取样本时长时间  毫秒
+    sample_time =  get_file_times(filepath)
+        #添加样本
+    add_samples(addsampleurl, "anquanjiashi" , file, sample_time)
+    print("完成样本" + "anquanjiashi" + "添加！" )
+
+def get_file_name(fielpath):
+    names = os.listdir(fielpath)
+    for name in names:
+        #获取到文件路径
+        filepath = path + name
+        # 打开文件
+        file = {'file': open(filepath, 'rb')}
+        #获取样本时长时间  毫秒
+        sample_time =  get_file_times(filepath)
+        #添加样本
+        add_samples(addsampleurl, name , file, sample_time)
+        print("完成样本" + name + "添加！" )
+
+def add_samples(url , samplename ,files , durationms):
 
     headers = {
-        'token' : '8a8af3a3-64cb-48b5-84f0-800370c05261'
+        'token' :  token
     }
-    filename  = 'baohujiayuan.mp4'
-    path = 'E:\\STE\\Xserver\\Xserver2.7\\ADSample\\' + filename
-    file = {'file': open(path, 'rb')}
-
     sample_data = {
         "id" : "" ,
-        "name" : "广告名称" ,
+        "name" : samplename ,
         "typeId" : 2004 ,
         "illegalIds" : "31" ,
         "advertisingRuleIds" : "2" ,
@@ -22,19 +60,76 @@ def addsample(url):
         "sourceServiceId" : 4921 ,
         "startTime" :  "2020-10-12" ,
         "endTime" : "2029-10-01" ,
-        "duration": 30000,
+        "duration": durationms,
         "sourceStartTime" : "2020-10-30 15:00:00" ,
         "sourceEndTime" : "2020-10-30 15:00:30" ,
         "remark"  : "样本添加",
         "onOff": 1,
         "back": 0
     }
-
-
-    # files = 1
-    # print(type(files))
-    res = requests.post(url , data  = sample_data , files = file , headers = headers)
+    res = requests.post(url , data  = sample_data , files = files , headers = headers)
     print(res.text)
 
+get_file_name(path)
 
-addsample('http://172.17.13.173:12521/advertisementSample/saveOrUpdateAdvertisementSample')
+# get_file_nameT(filename123)
+
+# import  requests , os
+#
+#
+# namelist  = []
+# path  = 'E:\\STE\\Xserver\\Xserver2.7\\ADSample\\'
+#
+# # def getfilenames(path):
+# #     names = os.listdir(path)
+# #     # for name in names:
+# #     #     namelist.append(name)
+# #     # print(namelist)
+# #     return names
+#
+# def getfile(path):
+# #     names = os.listdir(path)
+# #     pth = path
+# #     for name in names:
+# #         filepath = pth + name
+# #         # print(filepath)
+# #     file = {'file': open(filepath, 'rb')}
+#
+#
+#
+# getfile(path)
+#
+# def addsample(url , file):
+#     # url = 'http://172.17.13.172:12580/xserver/station/uploadStationPictures?stationId=1160'
+#
+#     headers = {
+#         'token' : '8a8af3a3-64cb-48b5-84f0-800370c05261'
+#     }
+#
+#     sample_data = {
+#         "id" : "" ,
+#         "name" : "真人秀2" ,
+#         "typeId" : 2004 ,
+#         "illegalIds" : "31" ,
+#         "advertisingRuleIds" : "2" ,
+#         "legal" : 0  ,
+#         "serviceIds" : "4921",
+#         "sourceServiceId" : 4921 ,
+#         "startTime" :  "2020-10-12" ,
+#         "endTime" : "2029-10-01" ,
+#         "duration": 1196264,
+#         "sourceStartTime" : "2020-10-30 15:00:00" ,
+#         "sourceEndTime" : "2020-10-30 15:20:00" ,
+#         "remark"  : "样本添加",
+#         "onOff": 1,
+#         "back": 0
+#     }
+#
+#
+#     # files = 1
+#     # print(type(files))
+#     res = requests.post(url , data  = sample_data , files = file , headers = headers)
+#     print(res.text)
+#
+#
+# # addsample('http://172.17.13.173:12521/advertisementSample/saveOrUpdateAdvertisementSample')
